cb6ef61e8de4313c1af9e0ba54932d3910c45b72
LeonardoDacosta29 <dacostaleonardo137@gmail.com>
2024-07-16 17:52:29 +0700

    web responsive untuk wira taxi, permulaan instal vue 3 dan bootstrap 5

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h1>Halo, Vue.js dan Bootstrap 5!</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld'
+};
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
